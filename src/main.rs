1fb3b02c47a2584f081d547f108324dd6bc7c7f1
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2021-06-24 20:09:55 -0500

    Add config list argument, reformat configuration logic, and format code

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir;
 use std::path::PathBuf;
 
 use chrono::Utc;
-use clap::{Arg, App};
+use clap::{App, Arg, ArgMatches};
 use dirs::home_dir;
 use preferences::{AppInfo, Preferences, PreferencesMap};
 use reqwest;
@@ -13,7 +13,10 @@ use tokio::io::AsyncWriteExt;
 
 const AUTHOR: &str = "RonquilloAeon";
 const API_BASE_URL: &str = "https://cryptopanic.com/api/v1";
-const APP_INFO: AppInfo = AppInfo { name: "CryptoPanicPortfolioFetcher", author: AUTHOR };
+const APP_INFO: AppInfo = AppInfo {
+    name: "CryptoPanicPortfolioFetcher",
+    author: AUTHOR,
+};
 const PREFERENCES_KEY: &str = "config";
 
 async fn fetch_portfolio(api_token: &String) -> Result<String, std::io::Error> {
@@ -22,9 +25,11 @@ async fn fetch_portfolio(api_token: &String) -> Result<String, std::io::Error> {
         .get(format!("{}/portfolio/", API_BASE_URL))
         .query(&[("auth_token", api_token)])
         .send()
-        .await.expect("Error fetching")
+        .await
+        .expect("Error fetching")
         .text()
-        .await.expect("Error getting response text");
+        .await
+        .expect("Error getting response text");
 
     Ok(data)
 }
@@ -41,32 +46,36 @@ async fn save_portfolio(data: String, data_dir: PathBuf) -> Result<(), std::io::
     values.insert("date".to_string(), serde_json::Value::String(utc_now));
 
     // Save to file
-    let path = data_dir.as_path().join(file_name).to_string_lossy().to_string();
+    let path = data_dir
+        .as_path()
+        .join(file_name)
+        .to_string_lossy()
+        .to_string();
     let mut file = File::create(&path).await?;
-    file.write_all(serde_json::to_string(&values).unwrap().as_bytes()).await?;
+    file.write_all(serde_json::to_string(&values).unwrap().as_bytes())
+        .await?;
     println!("Data saved to {}!", path);
 
     Ok(())
 }
 
 fn get_preferences() -> PreferencesMap {
-    let result = PreferencesMap::
-        <String>::load(&APP_INFO, PREFERENCES_KEY);
+    let result = PreferencesMap::<String>::load(&APP_INFO, PREFERENCES_KEY);
 
     return match result {
         Ok(prefs) => prefs,
         Err(_) => PreferencesMap::new(),
-    }
+    };
 }
 
 fn get_data_dir(prefs: &PreferencesMap) -> Result<PathBuf, std::io::Error> {
     let path = match prefs.contains_key("data_dir") {
-        true => PathBuf::from(prefs.get("data_dir").unwrap()),
+        true => PathBuf::from(prefs.get("data-dir").unwrap()),
         false => {
             let mut p = home_dir().unwrap();
             p.push("CryptoPanicData");
             p
-        },
+        }
     };
 
     if !path.exists() {
@@ -76,46 +85,75 @@ fn get_data_dir(prefs: &PreferencesMap) -> Result<PathBuf, std::io::Error> {
     Ok(path)
 }
 
+fn manage_configuration(prefs: &mut PreferencesMap, matches: &ArgMatches) {
+    // Handle changes
+    let mut changed = false;
+
+    if matches.is_present("api_token") {
+        prefs.insert(
+            "api-token".into(),
+            matches.value_of("api_token").unwrap().into(),
+        );
+        changed = true;
+    }
+
+    if matches.is_present("data_dir") {
+        let data_dir = matches.value_of("data_dir").unwrap();
+        prefs.insert("data-dir".into(), data_dir.into());
+        changed = true;
+    }
+
+    if changed {
+        let result = prefs.save(&APP_INFO, PREFERENCES_KEY);
+        assert!(result.is_ok());
+    }
+
+    // Optionally list preferences
+    if matches.is_present("list") {
+        for pref in prefs.into_iter() {
+            println!("{}={}", pref.0, pref.1)
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let mut prefs = get_preferences();
     let matches = App::new(APP_INFO.name)
         .author(APP_INFO.author)
-        .subcommand(App::new("configure")
-            .about("Configure the application")
-            .arg(Arg::new("api_token")
-                .short('t')
-                .long("apitoken")
-                .value_name("API_TOKEN")
-                .multiple(true)
-            )
-            .arg(Arg::new("data_dir")
-                .short('d')
-                .long("datadir")
-                .value_name("DATA_DIR")
-                .takes_value(true)
-            )
+        .subcommand(
+            App::new("configure")
+                .about("Configure the application")
+                .arg(
+                    Arg::new("data_dir")
+                        .short('d')
+                        .long("data-dir")
+                        .value_name("DATA_DIR")
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::new("list")
+                        .short('l')
+                        .long("list")
+                        .value_name("LIST")
+                        .takes_value(false),
+                )
+                .arg(
+                    Arg::new("api_token")
+                        .short('t')
+                        .long("api-token")
+                        .value_name("API_TOKEN")
+                        .multiple(true),
+                ),
         )
-        .subcommand(App::new("fetch")
-            .about("Fetch your portfolio")
-        )
+        .subcommand(App::new("fetch").about("Fetch your portfolio"))
         .get_matches();
 
     // Let's go!
     if let Some(ref matches) = matches.subcommand_matches("configure") {
-        if matches.is_present("api_token") {
-            prefs.insert("api_token".into(), matches.value_of("api_token").unwrap().into());
-        }
-
-        if matches.is_present("data_dir") {
-            let data_dir = matches.value_of("data_dir").unwrap();
-            prefs.insert("data_dir".into(), data_dir.into());
-        }
-
-        let result = prefs.save(&APP_INFO, PREFERENCES_KEY);
-        assert!(result.is_ok());
-    } else if let Some (_) = matches.subcommand_matches("fetch") {
-        if !prefs.contains_key("api_token") {
+        manage_configuration(&mut prefs, &matches)
+    } else if let Some(_) = matches.subcommand_matches("fetch") {
+        if !prefs.contains_key("api-token") {
             println!("Please set your API token using 'configure' command")
         } else {
             let data_dir = match get_data_dir(&prefs) {
@@ -123,11 +161,13 @@ async fn main() {
                 Err(e) => panic!("Error selecting data dir: {}", e),
             };
 
-            match fetch_portfolio(prefs.get("api_token").unwrap()).await {
+            match fetch_portfolio(prefs.get("api-token").unwrap()).await {
                 Ok(data) => {
-                    save_portfolio(data, data_dir).await.expect("Error saving to file");
-                },
-                Err(e) => println!("Error fetching data: {}", e)
+                    save_portfolio(data, data_dir)
+                        .await
+                        .expect("Error saving to file");
+                }
+                Err(e) => println!("Error fetching data: {}", e),
             }
         }
     }
